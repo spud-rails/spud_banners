9b8b95faf904f29e484f00151d5999a16f3ba241
Greg Woods <greg@westlakedesign.com>
2012-11-27 13:49:14 -0500

    seting up rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,40 @@
+# Track code coverage
+require 'simplecov'
+ SimpleCov.start 'rails' do
+  # root "dummy/"
+  add_filter "/factories/"
+end
+
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'database_cleaner'
+require 'shoulda'
+require 'factory_girl'
+require 'mocha'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
+Dir[File.join(ENGINE_RAILS_ROOT,"spec/support/**/*.rb"), File.join(ENGINE_RAILS_ROOT,"factories/*")].each {|f| require f}
+
+RSpec.configure do |config|
+  config.mock_with :mocha
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
--- spud_banners.gemspec
+++ spud_banners.gemspec
@@ -14,10 +14,19 @@ Gem::Specification.new do |s|
   s.description = "Spud Banners allows you to create and maintain sets of rotating banners for use on your Spud-based website."
 
   s.files = Dir["{app,config,db,lib}/**/*"] + ["MIT-LICENSE", "Rakefile", "Readme.markdown"]
-  s.test_files = Dir["test/**/*"]
+  s.test_files = Dir["spec/**/*"]
 
   s.add_dependency "rails", "~> 3.2.9"
   s.add_dependency 'spud_core', ">= 0.9.0"
   s.add_dependency "paperclip", ">= 0"
   s.add_dependency 'liquid'
+
+  s.add_development_dependency 'mysql2', '0.3.11'
+  s.add_development_dependency 'rspec', '2.8.0'
+  s.add_development_dependency 'rspec-rails', '2.8.1'
+  s.add_development_dependency 'shoulda', '~> 3.0.1'
+  s.add_development_dependency 'factory_girl', '2.5.0'
+  s.add_development_dependency 'mocha', '0.10.3'
+  s.add_development_dependency 'database_cleaner', '0.7.1'
+  s.add_development_dependency 'simplecov', '~> 0.6.4'
 end
