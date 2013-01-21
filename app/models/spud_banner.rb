e23758294cde828f9b94fbc9a869522dc77ba363
Greg Woods <greg@westlakedesign.com>
2013-01-21 09:51:19 -0500

    add inverse_of to banner associations

--- app/models/spud_banner.rb
+++ app/models/spud_banner.rb
@@ -1,6 +1,6 @@
 class SpudBanner < ActiveRecord::Base
   attr_accessible :banner, :link_to, :link_target, :title, :alt, :sort_order
-  belongs_to :owner, :class_name => 'SpudBannerSet', :foreign_key => 'spud_banner_set_id'
+  belongs_to :owner, :class_name => 'SpudBannerSet', :foreign_key => 'spud_banner_set_id', :inverse_of => :banners
 
   has_attached_file :banner, 
     :styles => lambda { |attachment| attachment.instance.dynamic_styles },
@@ -26,22 +26,6 @@ class SpudBanner < ActiveRecord::Base
       styles[:banner] = owner_style
     end
     return styles
-  end
-
-  def self.banners_for_set(identifier)
-    case identifier.class
-      when String
-        banner_set = SpudBannerSet.find_by_name(identifier)
-      when Symbol
-        banner_set = SpudBannerSet.find_by_name(identifier.to_s.titleize)
-      when Number
-        banner_set = SpudBannerSet.find(identifier)
-    end
-    if banner_set
-      return banner_set.banners
-    else
-      return []
-    end
   end
 
   def set_name
